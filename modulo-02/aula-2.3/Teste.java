be49a9b1ec8ec823e1b7b2fd0c6611c5bd497ba0
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-05-20 16:29:07 -0300

    2.3 - algoritmos

---
+++ modulo-02/aula-2.3/Teste.java
@@ -0,0 +1,10 @@
+public class Teste {
+
+    public static void main(String[] args) {
+
+        int retanguloA = 5;
+        int retanguloB = 5;
+
+        System.out.println("Resultado = " + (retanguloA + retanguloB));
+    }
+}
