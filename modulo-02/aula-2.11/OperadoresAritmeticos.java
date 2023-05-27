25c7a36090ebc5bb987805fd1ea7e5ae3392082b
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-05-27 18:52:32 -0300

    2.11 - operadores aritméticos

---
+++ modulo-02/aula-2.11/OperadoresAritmeticos.java
@@ -0,0 +1,18 @@
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+
+        double numero1 = 50;
+        double numero2 = 78;
+
+        System.out.println("Adição = " + (numero1 + numero2));
+
+        System.out.println("Subtração = " + (numero1 - numero2));
+
+        System.out.println("Subtração = " + (numero2 - numero1));
+
+        System.out.println("Multiplicação = " + (numero1 * numero2));
+
+        System.out.println("Divisão = " + (numero1 / numero2));
+    }
+}
