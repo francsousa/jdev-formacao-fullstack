a41330a13610e571eeb7ce6d9a7eba7cecac3dbc
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-05-27 19:30:26 -0300

    2.12 - operadores relacionais

---
+++ modulo-02/aula-2.12/OperadoresRelacionais.java
@@ -0,0 +1,14 @@
+public class OperadoresRelacionais {
+
+    public static void main(String[] args) {
+
+        int numero1 = 50;
+        int numero2 = 10;
+
+        if (numero1 <= numero2) { /* Se forem iguais ENTÃO RETORNA TRUE */
+            System.out.println("Ebbba o número 1 é menor ou igual");
+        } else {
+            System.out.println("Número 1 não é menor nem igual");
+        }
+    }
+}
