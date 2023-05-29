54a54011bc5c315b5e2e4ff548c12a67462be771
Francisco Sousa <franciscoeds1987@hotmail.com>
2023-05-29 13:59:48 -0300

    2.13 - operadores lógicos

---
+++ modulo-02/aula-2.13/OperadoresLogicos.java
@@ -0,0 +1,13 @@
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+
+        boolean numero1 = true;
+        boolean numero2 = false;
+        boolean numero3 = true;
+
+        if (numero1 || numero2) {
+            System.out.println("entrou na condição");
+        }
+    }
+}
